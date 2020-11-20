8a696fd32a8ef5afb87e1a2db141fb4ff53a2d9b
davidruvolo51 <davidruvolo51@gmail.com>
2020-11-20 15:55:55 +0100

    updated Rscripts and push commands

--- index.sh
+++ index.sh
@@ -2,7 +2,7 @@
 # FILE: index.sh
 # AUTHOR: David Ruvolo
 # CREATED: 2020-11-18
-# MODIFIED: 2020-11-18
+# MODIFIED: 2020-11-20
 # PURPOSE: script for interacting with GONL Server
 # DEPENDENCIES: mcmd (molgenis commander); RScript
 # COMMENTS: https://go-nl-acc.gcc.rug.nl
@@ -12,36 +12,50 @@
 # "cmd + shift + enter"
 # ////////////////////////////////////////////////////////////////////////////
 
-# ~ 0 ~
-# init
-# pip3 install molgenis-commander               # install mcmd
-# pip3 install --upgrade molgenis-commander     # upgrade mcmd
-# mcmd --version         # test mcmd
-
-# mcmd config add host # initial time only
-mcmd config set host
-
 # download data (waiting for access)
 # https://molgenis26.gcc.rug.nl/downloads/gonl_public/variants/release5/
 
 # //////////////////////////////////////
 
 # ~ 1 ~
-# compile EMX package and entities
-rm -rf data/gonl_*
-Rscript -e "source('R/emx.R')"
+# Build EMX files
 
+Rscript -e "source('R/emx_create_01_pkg.R')"
+Rscript -e "source('R/emx_create_02_entities.R')"
+Rscript -e "source('R/emx_create_03_attribs.R')"
+Rscript -e "source('R/emx_create_04_static_content.R')"
 
 # //////////////////////////////////////
 
 # ~ 2 ~
-# import package
+# Get Content
 
+# logo: download and optimize
+curl "http://www.nlgenome.nl/wp-content/uploads/2016/08/copy-logo2.png" -o src/images/gonl_logo.png
+open -a "ImageOptim" src/images/gonl_logo.png
+
+
+# //////////////////////////////////////
+
+# ~ 3 ~
+# Push to Molgenis Server
+
+# config `mcmd`
+# pip3 install molgenis-commander               # install mcmd
+# pip3 install --upgrade molgenis-commander     # upgrade mcmd
+# mcmd --version              # test mcmd
+# mcmd config add host        # initial time only
+mcmd config set host
+mcmd ping
+
+# push EMX assets
 mcmd import -p data/sys_md_Package.tsv
-
 mcmd import -p data/gonl_attributes.tsv --as attributes --in gonl
 mcmd import -p data/gonl_entities.tsv --as entities --in gonl
+
+# push source data
 mcmd import -p ~/Downloads/gonl.chr22.snps_indels.r5.vcf --in gonl --as gonl_chr22
 
+# push content
 mcmd import -p data/sys_StaticContent.tsv
-mcmd add logo -p images/gonl_logo.png
+mcmd add logo -p src/images/gonl_logo.png
