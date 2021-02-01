8660f682cfa0c34ae23437a6e73486c0761d1159
davidruvolo51 <davidruvolo51@gmail.com>
2021-02-01 12:30:44 +0100

    restructured pubs emx to xlsx file

--- R/pubmed_01_init_emx.R
+++ R/pubmed_01_init_emx.R
@@ -9,13 +9,16 @@
 #' COMMENTS: NA
 #'////////////////////////////////////////////////////////////////////////////
 
+#' pkgs
+suppressPackageStartupMessages(library(dplyr))
+
 #' ~ 1 ~
 #' create emx structure
 emx <- list()
 
 #' define package
 emx$package <- data.frame(
-    id = "publications",
+    name = "publications",
     label = "GoNL Publications",
     description = "GoNL Publications"
 )
@@ -49,7 +52,7 @@ emx$attributes <- tibble::tribble(
     "queries", "query",           "query to run",               "string"
 ) %>%
     mutate(
-        # entity = paste0("publications_", entity),
+        entity = paste0("publications_", entity),
         idAttribute = case_when(
             name %in% c("id", "uid") ~ TRUE,
             TRUE ~ FALSE
@@ -60,32 +63,32 @@ emx$attributes <- tibble::tribble(
         )
     )
 
+#' load data files
+emx$records <- read.csv("data/pubdata/records.csv")
+emx$queries <- read.csv("data/pubdata/queries.csv")
+
 #'//////////////////////////////////////
 
 #' ~ 2 ~
-#' Save
+#' Build EMX file
+
+#' init workboox
+wb <- openxlsx::createWorkbook()
+openxlsx::addWorksheet(wb, "packages")
+openxlsx::addWorksheet(wb, "entities")
+openxlsx::addWorksheet(wb, "attributes")
+openxlsx::addWorksheet(wb, "publications_records")
+openxlsx::addWorksheet(wb, "publications_queries")
+
+#' write data
+openxlsx::writeData(wb, "packages", emx$package)
+openxlsx::writeData(wb, "entities", emx$entities)
+openxlsx::writeData(wb, "attributes", emx$attributes)
+openxlsx::writeData(wb, "publications_records", emx$records)
+openxlsx::writeData(wb, "publications_queries", emx$queries)
+
+#' save workbook
+openxlsx::saveWorkbook(wb, "data/pubdata/publications.xlsx", overwrite = TRUE)
 
 #' write emx package
-write.csv(emx$package, "data/pubdata/sys_md_Package.csv", row.names = FALSE)
-
-#' save emx entities -- this doesn't work at the moment!
-write.csv(emx$entities, "data/pubdata/entities.csv", row.names = FALSE)
-write.csv(
-    emx$entities,
-    "data/pubdata/publications_entities.csv",
-    row.names = FALSE
-)
-
-#' save attributes for publication records entity
-write.csv(
-    emx$attributes[emx$attributes$entity == "records", ],
-    "data/pubdata/records_attributes.csv",
-    row.names = FALSE
-)
-
-#' save attributes for pubmed API queries entity
-write.csv(
-    emx$attributes[emx$attributes$entity == "queries", ],
-    "data/pubdata/queries_attributes.csv",
-    row.names = FALSE
-)
+# write.csv(emx$package, "data/pubdata/sys_md_Package.csv", row.names = FALSE)
