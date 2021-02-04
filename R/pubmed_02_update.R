2283c1f0b102a8716946183ee36cfb9404d17a74
davidruvolo51 <davidruvolo51@gmail.com>
2021-02-04 15:34:17 +0100

    init gh action

--- R/pubmed_02_update.R
+++ R/pubmed_02_update.R
@@ -22,17 +22,23 @@ ref_df <- readr::read_tsv("data/pubdatda/publications_history.tsv")
 papers <- list()
 
 #' define queries here
-papers$queries <- data.frame(
-    id = c("q_01", "q_02"),
-    type = c("author", "papers"),
-    query = c(
-        "\"Genome of the Netherlands consortium\"[Corporate Author]",
-        "The Genome of the Netherlands: design, and project goals[Title]"
-    )
-)
+#'papers$queries <- data.frame(
+#'    id = c("q_01", "q_02"),
+#'    type = c("author", "papers"),
+#'    query = c(
+#'        "\"Genome of the Netherlands consortium\"[Corporate Author]",
+#'        "The Genome of the Netherlands: design, and project goals[Title]"
+#'    )
+#')
 
 #' save queries
-#' write.csv(papers$queries, "data/pubdata/queries.csv", row.names = FALSE)
+papers$queries <- write.csv(
+    papers$queries, "data/pubdata/queries.csv", row.names = FALSE
+)
+
+if (length(papers$ids) == 0) {
+    print("No new papers")
+}
 
 #' get publication ids for each query
 papers$ids <- unlist(lapply(papers$queries$query, function(x) {
@@ -59,12 +65,12 @@ if (length(papers$ids)) {
     pubs <- pubmed$build_df(x = papers$data)
 
     #' build and write html (first time only)
-    html <- c("<ol>", pubmed$build_html(x = pubs), "</ol>")
-    pubmed$write_html(
-        file = "src/apps/publications/index.html",
-        id = "publicationList",
-        html = html
-    )
+    #' html <- c("<ol>", pubmed$build_html(x = pubs), "</ol>")
+    #' pubmed$write_html(
+    #'     file = "src/apps/publications/index.html",
+    #'     id = "publicationList",
+    #'     html = html
+    #' )
 
     #' save data
     write.csv(pubs, "data/pubdata/records.csv", row.names = FALSE)
